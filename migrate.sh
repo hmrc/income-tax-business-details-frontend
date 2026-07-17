a0e7b917ee35244d41123e65bad7d7f65f2582ff
kSiadat <49532979+kSiadat@users.noreply.github.com>
2026-07-17 16:51:32 +0100

    MIPR-2870: removed unused feature switches
    # Conflicts:
    #	app/common/models/admin/FeatureSwitchName.scala
    #	conf/application.conf
    #	test/common/auth/actions/FeatureSwitchRetrievalActionSpec.scala
    #	test/common/config/featureswitch/FeatureSwitchingSpec.scala

--- migrate.sh
+++ migrate.sh
@@ -11,7 +11,9 @@ cp app/businessDetails/models/SensitiveUIJourneySessionData.scala tmp/.
 cp app/shared/models/UIJourneySessionData.scala tmp/.
 cp app/common/models/obligations/ObligationsResponseModel.scala tmp/.
 cp -r app/shared/enums/journeyType tmp/.
-cp
+cp app/common/connectors/FeatureSwitchConnector.scala tmp/.
+cp app/common/models/admin/FeatureSwitchName.scala tmp/.
+cp app/common/services/admin/FeatureSwitchService.scala tmp/.
 
 echo "Removing main code from the app folder"
 rm -rf app/common
@@ -32,6 +34,9 @@ cp tmp/SensitiveUIJourneySessionData.scala app/businessDetails/models/.
 cp tmp/UIJourneySessionData.scala app/shared/models/.
 cp tmp/ObligationsResponseModel.scala app/common/models/obligations/.
 cp -r tmp/journeyType app/shared/enums/.
+cp tmp/FeatureSwitchConnector.scala app/common/connectors/.
+cp tmp/FeatureSwitchName.scala app/common/models/admin/.
+cp tmp/FeatureSwitchService.scala app/common/services/admin/.
 
 echo "Removing unused files"
 rm -rf app/shared/implicits
@@ -45,6 +50,11 @@ echo "Starting cut over of unit tests from income-tax-view-change-frontend"
 echo "Copying files to tmp folder which we don't want to overwrite"
 
 cp test/common/models/liabilityCalculation/LiabilityCalculationResponseModelSpec.scala tmp/.
+cp test/common/config/featureswitch/FeatureSwitchingSpec.scala tmp/.
+cp test/common/admin/FeatureSwitchNameSpec.scala tmp/.
+cp test/common/auth/actions/FeatureSwitchRetrievalActionSpec.scala tmp/.
+cp test/common/services/admin/FeatureSwitchServiceSpec.scala tmp/.
+cp test/common/mocks/connectors/MockFeatureSwitchConnector.scala tmp/.
 
 echo "Removing current unit tests"
 
@@ -62,6 +72,12 @@ cp -r ../income-tax-view-change-frontend/test/resources test/.
 echo "Copying files back from tmp folder to test folder"
 
 cp tmp/LiabilityCalculationResponseModelSpec.scala test/common/models/liabilityCalculation/.
+cp tmp/FeatureSwitchingSpec.scala test/common/config/featureswitch/.
+cp tmp/FeatureSwitchNameSpec.scala test/common/admin/.
+cp tmp/FeatureSwitchRetrievalActionSpec.scala test/common/auth/actions/.
+cp tmp/FeatureSwitchServiceSpec.scala test/common/services/admin/.
+cp tmp/MockFeatureSwitchConnector.scala test/common/mocks/connectors/.
+
 echo "Removing unused files"
 
 rm -rf test/shared/implicits
